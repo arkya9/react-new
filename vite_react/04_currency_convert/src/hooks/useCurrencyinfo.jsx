da27a0df1233df21da3ad3239028f01c0bc37bd4
arkya9 <arkoapple@gmail.com>
2024-06-19 23:33:15 +0530

    new

--- vite_react/04_currency_convert/src/hooks/useCurrencyinfo.jsx
+++ vite_react/04_currency_convert/src/hooks/useCurrencyinfo.jsx
@@ -1,25 +1,16 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react"
 
-function useCurrencyInfo(currency) {
-  const [data, setData] = useState({});
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
-        );
-        const result = await response.json();
-        setData(result); // Set the entire result to data
-      } catch (error) {
-        console.error("Error fetching currency data:", error);
-      }
-    };
-
-    fetchData();
-  }, [currency]);
-
-  return data;
+function useCurrencyInfo(currency){
+    const [data, setData] = useState({})
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        .then((res) => res.json())
+        .then((res) => setData(res[currency]))
+        console.log(data);
+    }, [currency])
+    console.log(data);
+    return data
 }
 
 export default useCurrencyInfo;
