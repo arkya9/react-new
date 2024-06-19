2399d7803a71682be800c56579e4e190aae008e2
arkya9 <arkoapple@gmail.com>
2024-06-19 22:10:23 +0530

    new

---
+++ vite_react/04_currency_convert/src/hooks/useCurrencyinfo.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+        );
+        const result = await response.json();
+        setData(result); // Set the entire result to data
+      } catch (error) {
+        console.error("Error fetching currency data:", error);
+      }
+    };
+
+    fetchData();
+  }, [currency]);
+
+  return data;
+}
+
+export default useCurrencyInfo;
